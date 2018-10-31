70efaadcda851e014c82203aca996d7d21d33f2c
Jon Layton <me@jonl.io>
2018-10-30 20:02:33 -0500

    Bitcoin-Qt -> BSHA3-Qt
    -BEGIN VERIFY SCRIPT-
    
    git grep -l "Bitcoin-Qt" . | xargs gsed -i "s/Bitcoin-Qt/BSHA3-Qt/g"
    
    -END VERIFY SCRIPT-

--- src/qt/guiconstants.h
+++ src/qt/guiconstants.h
@@ -48,7 +48,7 @@ static const int MAX_URI_LENGTH = 255;
 
 #define QAPP_ORG_NAME "Bitcoin"
 #define QAPP_ORG_DOMAIN "bitcoin.org"
-#define QAPP_APP_NAME_DEFAULT "Bitcoin-Qt"
-#define QAPP_APP_NAME_TESTNET "Bitcoin-Qt-testnet"
+#define QAPP_APP_NAME_DEFAULT "BSHA3-Qt"
+#define QAPP_APP_NAME_TESTNET "BSHA3-Qt-testnet"
 
 #endif // BITCOIN_QT_GUICONSTANTS_H
