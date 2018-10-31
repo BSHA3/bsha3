f9ea34ca051d67b605b0a6e336227ebdf7b948eb
Jon Layton <me@jonl.io>
2018-10-31 17:06:46 -0500

    fixup: tests: Build fixes for removed ALWAYS_ACTIVE

--- src/test/test_bitcoin.cpp
+++ src/test/test_bitcoin.cpp
@@ -61,7 +61,7 @@ BasicTestingSetup::BasicTestingSetup(const std::string& chainName)
     fCheckBlockIndex = true;
     // CreateAndProcessBlock() does not support building SegWit blocks, so don't activate in these tests.
     // TODO: fix the code to support SegWit blocks.
-    gArgs.ForceSetArg("-vbparams", strprintf("segwit:0:%d", (int64_t)Consensus::BIP9Deployment::NO_TIMEOUT));
+    //gArgs.ForceSetArg("-vbparams", strprintf("segwit:0:%d", (int64_t)Consensus::BIP9Deployment::NO_TIMEOUT));
     SelectParams(chainName);
     noui_connect();
 }
