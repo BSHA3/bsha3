b0208b90f7f55d1ff2bcf755c884a9795d6d5e88
Kian <fafrd@horsefucker.org>
2018-11-03 19:10:03 -0400

    Detangle wallet.cpp/h includes, use extern instead
    This fixes the issue where tests weren't building

--- src/wallet/wallet.cpp
+++ src/wallet/wallet.cpp
@@ -37,9 +37,6 @@
 
 static const size_t OUTPUT_GROUP_MAX_ENTRIES = 10;
 
-static CCriticalSection cs_wallets;
-static std::vector<std::shared_ptr<CWallet>> vpwallets GUARDED_BY(cs_wallets);
-
 bool AddWallet(const std::shared_ptr<CWallet>& wallet)
 {
     LOCK(cs_wallets);
--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -39,6 +39,9 @@ bool HasWallets();
 std::vector<std::shared_ptr<CWallet>> GetWallets();
 std::shared_ptr<CWallet> GetWallet(const std::string& name);
 
+static CCriticalSection cs_wallets;
+static std::vector<std::shared_ptr<CWallet>> vpwallets GUARDED_BY(cs_wallets);
+
 //! Default for -keypool
 static const unsigned int DEFAULT_KEYPOOL_SIZE = 1000;
 //! -paytxfee default
